62b45094ed96aef1992c2b055036de18d30f5cdf
10230035 <10230035@B207PC06>
2026-03-13 12:29:13 +0300

    Добавьте файлы проекта.

---
+++ Store/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Models;
+
+namespace Store.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
---
+++ Store/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.Models
+{
+    public class Role
+    {
+        [Key]
+        public int IdRole { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? Name { get; set; }
+        public ICollection<User>? Users { get; set; }
+
+    }
+}
