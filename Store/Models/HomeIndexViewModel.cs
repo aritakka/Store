39a69f0f33cf324a33025ecf67665dcb93336caa
aritakka <normymagic1@gmail.com>
2026-03-27 14:17:25 +0300

    1

---
+++ Store/Models/HomeIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Store.Models
+{
+    public class HomeIndexViewModel
+    {
+        public List<Product> Products { get; set; } = new();
+        public Product? Featured { get; set; }
+    }
+}
